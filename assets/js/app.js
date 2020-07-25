99c5992fbedf1aac193a7003996cf5ade870a517
Omotola Macaulay <Tmac-thegreat@github.io>
2020-07-26 00:52:27 +0100

    fix footer icons

--- assets/js/app.js
+++ assets/js/app.js
@@ -52,71 +52,8 @@ var swiper = new Swiper('.swiper-container', {
 	}
 });
 
-// $(document).ready(function(){
-// 	$('.slick-slider').slick({
-		
-// 	});
-// });
-// $('.responsive').slick({
-//   dots: true,
-//   infinite: false,
-//   speed: 300,
-//   slidesToShow: 4,
-//   slidesToScroll: 4,
-//   responsive: [
-//     {
-//       breakpoint: 1024,
-//       settings: {
-//         slidesToShow: 3,
-//         slidesToScroll: 3,
-//         infinite: true,
-//         dots: true
-//       }
-//     },
-//     {
-//       breakpoint: 600,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 2
-//       }
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1
-//       }
-//     }
 
-$(document).ready(function(){
-  $('#slide .slider').slick({
-  //   autoplay: true,
-  //   autoplaySpeed: 2000,
-  //   dots: true,
-  //   speed: 2000,
-  //   centerMode: true,
-  // centerPadding: '60px',
-  //   slidesToShow: 2,
-  //   responsive: [
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //         arrows: false,
-  //         centerMode: true,
-  //         centerPadding: '40px',
-  //         slidesToShow: 4
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         arrows: false,
-  //         centerMode: true,
-  //         centerPadding: '40px',
-  //         slidesToShow: 1
-  //       }
-  //     }
-  //   ]
-  });
-});
 new WOW().init();
+const scroll = new SmoothScroll('.navbar-nav a[href*="#"]', {
+	speed: 500
+});
