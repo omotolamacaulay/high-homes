924ce53e7a8bf9456d37d2b277241a9fa67303f3
Omotola Macaulay <Tmac-thegreat@github.io>
2020-07-25 21:00:06 +0100

    add testimonial slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,24 +2,24 @@
 var swiper = new Swiper('.swiper-container', {
 	effect: 'coverflow',
 	grabCursor: true,
-	centeredSlides: true,
-	slidesPerView: 'auto',
-	spaceBetween: 0,
+	centeredSlides: false,
+	slidesToScroll: '1',
+	// slidesPerView: '2',
+	spaceBetween: 30,
 	coverflowEffect: {
 		rotate: 0,
 		stretch: 0,
-		depth: 190,
+		depth:0,
 		modifier: 1,
 		slideShadows: false,
 	},
-	centeredSlides: true,
 	autoplay: {
 		delay: 2500,
 		disableOnInteraction: false,
 	},
 	loop: true,
 	freeMode: true,
-	loopedSlides: 5,
+	loopedSlides: 4,
 	pagination: {
 		el: '.swiper-pagination',
 		clickable: true,
@@ -28,13 +28,35 @@ var swiper = new Swiper('.swiper-container', {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
+	breakpoints: {
+		256: {
+			slidesPerView: 1,
+			slidesToScroll: '1',
+			spaceBetween: 0,
+		},
+		576: {
+			slidesPerView: 1,
+			slidesToScroll: '1',
+			spaceBetween: 0,
+		},
+		768: {
+			slidesPerView: 1,
+			slidesToScroll: '1',
+			spaceBetween: 0,
+		},
+		992: {
+			slidesPerView: 2,
+			slidesToScroll: '1',
+			spaceBetween: 10,
+		},
+	}
 });
 
-$(document).ready(function(){
-	$('.slick-slider').slick({
+// $(document).ready(function(){
+// 	$('.slick-slider').slick({
 		
-	});
-});
+// 	});
+// });
 // $('.responsive').slick({
 //   dots: true,
 //   infinite: false,
@@ -67,35 +89,34 @@ $(document).ready(function(){
 //     }
 
 $(document).ready(function(){
-  // $('#slide .slider').slick({
-    autoplay: true,
-    autoplaySpeed: 2000,
-    dots: true,
-    speed: 2000,
-    centerMode: true,
-  centerPadding: '60px',
-    slidesToShow: 2,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '20px',
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '20px',
-          slidesToShow: 1
-        }
-      }
-    ]
+  $('#slide .slider').slick({
+  //   autoplay: true,
+  //   autoplaySpeed: 2000,
+  //   dots: true,
+  //   speed: 2000,
+  //   centerMode: true,
+  // centerPadding: '60px',
+  //   slidesToShow: 2,
+  //   responsive: [
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //         arrows: false,
+  //         centerMode: true,
+  //         centerPadding: '40px',
+  //         slidesToShow: 4
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         arrows: false,
+  //         centerMode: true,
+  //         centerPadding: '40px',
+  //         slidesToShow: 1
+  //       }
+  //     }
+  //   ]
   });
 });
-
 new WOW().init();
